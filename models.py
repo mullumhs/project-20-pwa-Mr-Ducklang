758152d56cd577f8116115b14c4f72b868701e7a
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-11-20 10:39:01 +1100

    Created Database model called Vehicle

--- models.py
+++ models.py
@@ -3,4 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Define your database model here
-# Example: class Item(db.Model):
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    type = db.Column(db.String(100), nullable=True)
+    make = db.Column(db.String(100), nullable=False)
+    model = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable = True)
+
+    def __repr__(self):
+        return f'<Task {self.Title}>'
